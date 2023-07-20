cedf2e09c155cf44c222864a1cdaed018bb9b7f6
Jose Zaupa <jzaupa@commandalkon.com>
2023-07-19 21:37:55 -0300

    Finalizando o projeto

---
+++ Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note.Models
+{
+    class About
+    {
+        public string Title => AppInfo.Name;
+		public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Este aplicativo é escrito em XML e C# com .Net Maui! - Lide com isso";
+	}
+}
